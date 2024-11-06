019c43f4969eff1c8a00ff1fea92a05df6d34270
Kaio Pablo Carvalho Mendes <kaiopablo17@gmail.com>
2024-11-05 23:54:46 -0300

    commented code

--- lib/check_site_status.ex
+++ lib/check_site_status.ex
@@ -1,40 +1,41 @@
 defmodule CheckSiteStatus do
-  use GenServer
+  use GenServer # Permite a atuação como um OTP
   use HTTPoison.Base
 
   @check_interval 5_000
 
-  def start_link(_) do
-    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  def start_link(_) do # Iniciando o GenServer
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__) # Próprio módulo , mapa vazio no estado inicial e o nome do módulo
   end
 
-  def init(state) do
-    schedule_check()
-    {:ok, state}
+  def init(state) do # É chamada quando o GenServer é iniciado
+    schedule_check() # Agenda a verificação
+    {:ok, state} # Indica que o processo iniciou vazio retornando o state
   end
 
-  def handle_info(:check_google, state) do
+  def handle_info(:check_google, state) do # Chamada quando o GenServer recebe a mensagem :check_google
     check_google()
     schedule_check()
-    {:noreply, state}
+    {:noreply, state} # O processo não precisa responder a nenhum outro e mantém o estado atual
   end
 
   defp check_google do
+    #          hora utc atual |> converte retirando o id utc |> Remove as frações dos segs   |> Converte para string
     timestamp = DateTime.utc_now() |> DateTime.to_naive() |> NaiveDateTime.truncate(:second) |> NaiveDateTime.to_string()
 
-    case HTTPoison.get("https://www.google.com") do
+    case HTTPoison.get("https://www.google.com") do # Requisição get
       {:ok, %HTTPoison.Response{status_code: 200}} ->
         IO.inspect("[#{timestamp}] O Google está online!")
 
       {:ok, %HTTPoison.Response{status_code: status_code}} ->
-        IO.inspect("O Google respondeu com status #{status_code}, mas pode não estar totalmente acessível.")
+        IO.inspect("[#{timestamp}] O Google respondeu com status #{status_code}, mas pode não estar totalmente acessível.")
 
       {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect("Falha ao acessar o Google: #{reason}")
+        IO.inspect("[#{timestamp}] Falha ao acessar o Google: #{reason}")
     end
   end
 
   defp schedule_check do
-    Process.send_after(self(), :check_google, @check_interval)
+    Process.send_after(self(), :check_google, @check_interval) # Envia a mensagem :check_google para o próprio processo com o intervalo definido.
   end
 end
